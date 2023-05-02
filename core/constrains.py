53bfb6eb312e7361c71f9bb02098faa4f6f0505b
Claus <matheuscdd@hotmail.com>
2023-05-02 11:17:03 -0300

    feat: initial commit

---
+++ core/constrains.py
@@ -0,0 +1,32 @@
+NAME = "name"
+IMAGE = "image"
+GENDER = "gender"
+ABOUT = "about"
+YEAR = "year"
+COPIES = "copies"
+COPY = "Copy"
+AUTHORS = "authors"
+AUTHOR = "Author"
+USERS = "users"
+USER = "User"
+BOOKS = "books"
+BOOK = "Book"
+BOOK_LOAN = "BookLoan"
+EMAIL = "email"
+USERNAME = "username"
+LOAN_DATE = "loan_date"
+RETURN_DATE = "return_date"
+ISBN = "ISBN"
+ASIN = "ASIN"
+JSON = "json"
+ID = "id"
+WRITE_ONLY = {"write_only": True}
+DEFAULT = "default"
+SERIES = "series"
+POSTGRES_ = "POSTGRES_"
+FIRST_NAME = "first_name"
+LAST_NAME = "last_name"
+
+
+def way(arg1: str, arg2: str) -> str:
+    return arg1 + "." + arg2
