e3f6bdc5a8921092f3a79cdf5d9350bf08e42e3b
matheuscdd <matheuscdd@hotmail.com>
2023-05-09 12:53:14 -0300

    feat: working on model user

--- tests/models/users/test.py
+++ tests/models/users/test.py
@@ -25,6 +25,21 @@ class TestClassUser(TestCase):
         cls.worker_data = create_user_data("Worker", "work", is_colaborator=True)
         cls.worker_instance = User.objects.create_user(**cls.worker_data)
 
-    def test_user_fields(self):
+    def test_user_adm_fields(self):
         """Testing if all model fields are correctly added to database"""
-        fields = [USERNAME, EMAIL, IS_SUPERUSER, IS_COLABORATOR]
+        fields = [USERNAME, EMAIL, IS_SUPERUSER, IS_COLABORATOR, IS_STUDENT]
+        for field in fields:
+            result = getattr(self.adm_instance, field)
+            expected = self.adm_data[field]
+            msg = f'Checking if {field} is the same'
+            self.assertEqual(result, expected, msg)
+
+        result = self.adm_instance.password
+        expected = self.adm_data[PASSWORD]
+        msg = 'Checking if password is hashed'
+        self.assertNotEqual(result, expected, msg)
+
+        result = self.adm_instance.check_password(self.adm_data[PASSWORD])
+        msg = 'Checking if the password is hashed correctly'
+        self.assertTrue(result, msg)
+        
